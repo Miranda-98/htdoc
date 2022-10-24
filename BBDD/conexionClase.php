cbf51be9cf403ab86a5992897def366db91e80f4
Gerardo <gerardomiranda2498@gmail.com>
2022-10-24 11:38:50 +0200

    array js, falta el 3

---
+++ BBDD/conexionClase.php
@@ -0,0 +1,22 @@
+<?php 
+    function conectar(){
+        $servidor = "localhost";
+        $usuario = "root";
+        $contraseña = "";
+        $bd = "alumnos";
+
+        try{
+            $conexion = new PDO ("mysql:host=$servidor;dbname=$bd", $usuario, $contraseña);
+            $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "conexion correcta";
+        } catch(PDOException $e) {
+            echo "<br/>" . $e->getMessage();
+            $conexion = null;
+        }
+
+       
+        return $conexion;
+    }
+
+    conectar();
+?>
