1a9ceeb42f772a646259e2f0a2b176c0e648344b
Gerardo <gerardomiranda2498@gmail.com>
2022-10-25 14:12:55 +0200

    tarea bbdd modificar usuarios

---
+++ BBDD/Botonera BD/modificarUsuarios.php
@@ -0,0 +1,10 @@
+<?php 
+    /*
+        2 formularios
+        1 recibe lo que quiero cambiar -> lo busco en la base de datos
+            si esta devuelvo sus valores al formulario 2, si no esta
+            le aviso de que no se encuentra en la base de datos
+        formulario 2 muestra los valores de la base de datos con un value 
+        o placeholder los modifico y aplico los cambios en la base de datos
+    */
+?>
