56f4a2a7ee12b5d4062ea3a12b6610baa9edb15b
Gerardo <gerardomiranda2498@gmail.com>
2022-10-26 12:57:35 +0200

    modificar usuario funcional, falta terminar

--- BBDD/Botonera BD/modificarUsuarios.php
+++ BBDD/Botonera BD/modificarUsuarios.php
@@ -1,70 +1,57 @@
 <?php 
     echo "<link rel='stylesheet' type='text/css' href='estilosBotonera.css' />";
-    require "conexionClase.php";
-    /*
-        2 formularios
-        1 recibe lo que quiero cambiar -> lo busco en la base de datos
-            si esta devuelvo sus valores al formulario 2, si no esta
-            le aviso de que no se encuentra en la base de datos
-        formulario 2 muestra los valores de la base de datos con un value 
-        o placeholder los modifico y aplico los cambios en la base de datos
-    */
+    require_once 'conexionClase.php';
+    $datosCorrectos = 0;
+   
 
-
-    if($datosCorrectos == false){
-        echo "<div id='buscaUsuario'><form action='' method='post'><fieldset>
-            <legend>Completa los campos:</legend>
-            <input type='text' name='codigo' placeholder='Codigo'>
-            <p><input id='botonEnviar' type='submit' name='botonEnviar' value='Enviar datos'></p>
-            </fieldset></form></div>";
-    } else {
-        echo "<div id='insertaUsuario'><form action='modificarUsuarios2.php' method='post'>
-            <fieldset>
-            <legend>Completa los campos:</legend>
-            <input type='text' name='codigo' value=".$resultado['CODIGO'].">
-            <input type='text' name='nombre' value=".$resultado['NOMBRE'].">
-            <input type='text' name='apellido' value=".$resultado['APELLIDOS'].">
-            <input type='text' name='telefono' value=".$resultado['TELEFONO'].">
-            <input type='text' name='correo' value=".$resultado['CORREO'].">
-            <p><input type='submit' id='botonEnviar2' name='botonEnviar' value='Enviar datos'></p>
-            </fieldset></form></div>";
-    }
-
-    
     if(isset($_POST['botonEnviar'])){
         $codigoR = $_POST['codigo'];
     }
 
-    try{
+
+
+    try{    
+
         $conecta = conectar();
         $conecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $sqlP = $conecta->prepare("SELECT * FROM alumnos WHERE CODIGO = :codigo");
         $sqlP->bindParam(':codigo',$codigoR);
         $sqlP->execute();
 
+
         $resultado = $sqlP->fetch(PDO::FETCH_ASSOC);
-        echo var_dump($resultado);
         if($resultado <= 0){
             echo "el alumno no esta en la base de datos, no lo puedes modificar";
-            $datosCorrectos = false;
+            
         } else {
-            $datosCorrectos = true;
-            echo "<div id='insertaUsuario'><form action='modificarUsuarios2.php' method='post'>
-            <fieldset>
-            <legend>Completa los campos:</legend>
-            <input type='text' name='codigo' value=".$resultado['CODIGO'].">
-            <input type='text' name='nombre' value=".$resultado['NOMBRE'].">
-            <input type='text' name='apellido' value=".$resultado['APELLIDOS'].">
-            <input type='text' name='telefono' value=".$resultado['TELEFONO'].">
-            <input type='text' name='correo' value=".$resultado['CORREO'].">
-            <p><input type='submit' id='botonEnviar2' name='botonEnviar' value='Enviar datos'></p>
-            </fieldset></form></div>";
-            //  $url = "modificarUsuarios2.php";
-            //  echo "<SCRIPT>window.location='$url';</SCRIPT>";
+            $datosCorrectos = 1;
+            
         }
 
+
+        
     } catch (PDOException $e) {
-        echo "error: ".$e->getMessage;
+        echo 'error: '.$e->getMessage;
     }
+
+    
+if($datosCorrectos == 0 ){
+            echo "<div id='buscaUsuario'><form action='' method='post'><fieldset>
+                <legend>Completa los campos:</legend>
+                <input type='text' name='codigo' placeholder='Codigo'>
+                <p><input id='botonEnviar' type='submit' name='botonEnviar' value='Enviar datos'></p>
+                </fieldset></form></div>";
+        } else if($datosCorrectos == 1){
+            echo "<div id='insertaUsuario'><form action='modificarUsuarios2.php' method='post'>
+                <fieldset>
+                <legend>Completa los campos:</legend>
+                <input type='text' name='codigo' value=".$resultado['CODIGO'].">
+                <input type='text' name='nombre' value=".$resultado['NOMBRE'].">
+                <input type='text' name='apellido' value=".$resultado['APELLIDOS'].">
+                <input type='text' name='telefono' value=".$resultado['TELEFONO'].">
+                <input type='text' name='correo' value=".$resultado['CORREO'].">
+                <p><input type='submit' id='botonEnviar2' name='botonEnviar' value='Enviar datos'></p>
+                </fieldset></form></div>";
+        }
     
 ?>
