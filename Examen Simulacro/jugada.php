b1e85a0f225f952a7b7a07dd78c526a80d7e123c
Gerardo <gerardomiranda2498@gmail.com>
2022-11-03 09:05:59 +0100

    merge

--- Examen Simulacro/jugada.php
+++ Examen Simulacro/jugada.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-=======
 <?PHP
     if (isset($_POST['limiteInferior'])){
         $limInferior = $_POST['limiteInferior'];
@@ -52,7 +50,6 @@
 ?>
 
 
->>>>>>> 9a2a3ebf8bb4ccc9a3c731f2ffd663d36ff24bd4
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -65,56 +62,12 @@
     <form method="POST" action="">
         <label for="numUser">Introduce un número</label>
         <input type="number" name="num" value="<?php $num ?>" required></br>
-<<<<<<< HEAD
-        <input type="text" disabled value="<?php echo $_GET['msg'] ?? ''?>"></br>
-        <input type="text"  name="limINF" value="<?php echo $limInferior = $_POST['limiteInferior'] ?? '' ?>"><br/>
-        <input type="text"  name="limSUP" value="<?php echo $limSuperior = $_POST['limiteSuperior'] ?? '' ?>"><br/>
-        <input type="text"  name="rnd" value="<?php 
-        echo $valorRanda = rand($_POST['limiteInferior'],$_POST['limiteSuperior']) ?? 'vacio' ?>">
-=======
         <input type="text"  name="limINF" value="<?php echo $limInferior  ?>"><br/>
         <input type="text"  name="limSUP" value="<?php echo $limSuperior ?>"><br/>
         <input type="text"  name="rnd" value="<?php echo $valorRanda ?>">
         <input type="text"  name="intentos" value="<?php echo $intento ?>">
->>>>>>> 9a2a3ebf8bb4ccc9a3c731f2ffd663d36ff24bd4
         <input type="submit" name="botonEnviar2" value="OK!">
     </form>
 </body>
 </html>
 
-<<<<<<< HEAD
-<?PHP
-    if ($_POST['limiteInferior'] > $_POST['limiteSuperior']) {
-        header("location:limites.php ? msg='limites super ... inferior'");
-        die();
-    }   
-
-    if (isset($_POST['botonEnviar2'])) {
-        $num = $_POST['num'];
-        $limInferior = $_POST['limINF'];
-        $limSuperior = $_POST['limSUP'];
-        $rnd = $_POST['rnd'];
-
-        if($num == $rnd) {
-            header("location: acierto.php ");
-        } else if ($num < $rnd) {
-            echo "buscas un numero mayor";
-        } else {
-            echo "buscas un numero menor";
-        }
-
-    } else {
-        echo "erro en el fomrmulario";
-    }
-    
-    
-    
-    
-// } else {
-//     echo "erro en el fomrmulario";
-// }
-    
-    
-?>
-=======
->>>>>>> 9a2a3ebf8bb4ccc9a3c731f2ffd663d36ff24bd4
