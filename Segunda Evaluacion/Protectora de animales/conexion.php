f36479f7486119e863a6087709beb9ff14ad11e7
Miranda-98 <gerardomiranda2498@gmail.com>
2022-12-09 09:14:33 +0100

    nn

--- Segunda Evaluacion/Protectora de animales/conexion.php
+++ Segunda Evaluacion/Protectora de animales/conexion.php
@@ -46,12 +46,12 @@
         }
 
 
-         function conectar(){
+        protected function conectar(){
             try {
-                $conecta = new PDO("mysql:host=".$this->get_servidor().";dbname=".$this->get_bbdd()."",
-                 $this->get_usuario(), $this->get_contraseña());
+                $conecta = new PDO("mysql:host=".$this->get_servidor().";dbname=".$this->get_bbdd()."", $this->get_usuario(), $this->get_contraseña());
                 $conecta->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-                echo "conexion exitosa";
+                 echo "conexion exitosa";
+                // print_r($conecta);
                 return $conecta;
 
             } catch (PDOException $e) {
@@ -62,11 +62,36 @@
         
     }
 
-    // class main extends Conectar{
-    //     public $con = new Conectar("animales", "localhost", "root", "");
-    //     public $con->conectar();
+    class PruebaConexion extends Conexion{
+        function con(){
+            $this->conectar();
+        }
+    }
+    
+    // $obj = new PruebaConexion("animales", "localhost", "root", "");
+    // echo "<pre>"; 
+    // print_r($obj);
+    // echo "</pre>";
+
+    
+    // $obj->con();
+    // echo "<br/>scrip conexion.php<br/>";
+
+
+    // $obj = new PDO("mysql:host=localhost;dbname=animales", 'root', '');
+    // $obj->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    // $sql = "SELECT * FROM animales;";
+
+    // $result = $obj->query($sql);//almaceno en result lo que devuelve la query 
+    // foreach($result as $row){
+    //     echo $row[0],
+    //                 $row[1],
+    //                 $row[2],
+    //                 $row[3],
+    //                 $row[4].'<br/>';
     // }
-   
-    $con = new Conexion("animales", "localhost", "root", "");
-    $con->conectar();
+
+    // echo "<pre>"; 
+    // print_r($obj);
+    // echo "</pre>";
 ?>
--- Segunda Evaluacion/Protectora de animales/crud.php
+++ Segunda Evaluacion/Protectora de animales/crud.php
@@ -3,16 +3,16 @@
 
     abstract class Crud extends Conexion {
         private $tabla;
-        private $conexion;
+        private $con;
 
-        function __construct($tabla)
+        function __construct($tabla, $con)
         {   
             $this->tabla = $tabla;
-            $this->conexion = conectar();
+            $this->con = parent::__construct("animales","localhost","root","");
         }
 
         function get_tabla(){
-            return $this->bbdd;
+            return $this->tabla;
         }
 
         function set_tabla($tabla){
@@ -24,23 +24,77 @@
         }
 
         // no lo quiero para que no me puedan cambiar la conexion a la bbdd
-        // function set_conexion($conexion){
-        //     $this->conexion=$conexion;
-        // }
+        function set_conexion($conexion){
+            $this->conexion=$conexion;
+        }
+
+
 
         function obtieneTodos(){
-            $sql = ("SELECT * FROM ".$this->get_tabla().";");
-            $stm = $this->get_conexion()->prepare($sql);
-            $stm -> bind_param("?", $this->get_tabla());
-            $stm -> execute();
-            $result = $stm -> fetchObject();
+            try{
+                $conn=parent::conectar();
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql= "SELECT * FROM $this->tabla ";
+                $stmt = $conn->prepare($sql);
+                $stmt->execute();
+                $registros=$stmt->fetchAll(PDO::FETCH_OBJ);
+                return($registros);
 
-            for ($i=0; $i < count($this->get_tabla()); $i++) { 
-               echo " <tr>
-                <td>".$result[0]."</td>" ;
+            } catch (PDOException $e) {
+                echo "Error al obtener todos " . $e->getMessage();
             }
 
         }
+
+        function obtieneID($id){
+            try{
+                $conn = parent::conectar();
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //comprobar que el id existe
+                //si existe se muestra la informacion relacionada
+                $sqlComprobar = "SELECT COUNT(*) as 'cantidad' from $this->tabla where id = $id";
+                $resultado = $conn->query($sqlComprobar);
+                $num = $resultado->fetch();
+                if($num['cantidad']>0) {
+                    $sql= "SELECT * FROM $this->tabla WHERE id = $id ";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->execute();
+                    $registros=$stmt->fetchAll(PDO::FETCH_OBJ);
+                    return($registros);
+                } else {//si no existe muestra un mensaje
+                    echo "No existe el id indicado en la tabla " . $this->tabla;
+                }
+  
+            } catch (PDOException $e) {
+                echo "Error al obtener todos " . $e->getMessage();
+            }
+        }
+
+        function borrarID($id){
+            try{
+                $conn = parent::conectar();
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                //comprobar que el id existe
+                //si existe se muestra la informacion relacionada
+                $sqlComprobar = "SELECT COUNT(*) as 'cantidad' from $this->tabla where id = $id";
+                $resultado = $conn->query($sqlComprobar);
+                $num = $resultado->fetch();
+                if($num['cantidad']>0) {
+                    $sql= "DELETE FROM $this->tabla WHERE id = $id";
+                    $stmt = $conn->prepare($sql);
+                    $stmt->execute();
+                    echo $id ." eliminado correctamente de la tabla " .$this->tabla;
+                } else {//si no existe muestra un mensaje
+                    echo "No existe el id indicado en la tabla " . $this->tabla;
+                }
+  
+            } catch (PDOException $e) {
+                echo "Error al obtener todos " . $e->getMessage();
+            }
+        }
+
+        abstract function crear();
+        abstract function actualizar();
 
         function Create(){
 
@@ -59,10 +113,61 @@
         }
     }
 
-    class Main extends Crud {
+    class PruebaCrud extends Crud{
+        function crear()
+        {
+            $this->crear();
+        }
 
+        function actualizar()
+        {
+            $this->actualizar();
+        }
     }
-    $x = new Main("animales");
-    $x->obtieneTodos();
-    echo $x;
+
+    // echo "<br/>script crud.php<br/>";
+    // $x = new Conexion("animales", "localhost", "root", "");
+    // $pruebaCrud = new PruebaCrud('animales', $x);
+    // $pruebaCrud2 = new PruebaCrud('adopciones', $x);
+
+    // echo "<pre>";
+    // print_r($pruebaCrud);
+    // echo "</pre>";
+    
+    // echo "<h2>Obtiene Todos</h2>";
+    // $resultadoObitneTodos = $pruebaCrud->obtieneTodos();
+    // echo "<pre>";
+    // var_dump($resultadoObitneTodos);
+    // echo "</pre>";
+    
+
+    // echo "<h2>Obtiene ID</h2>";
+    // $resultadoObtieneID = $pruebaCrud->obtieneID('1');
+    // echo "<pre>";
+    // var_dump($resultadoObtieneID);
+    // echo "</pre>";
+
+    // echo "<h2>Borrar ID</h2>";
+    // $resultadoBorrarID = $pruebaCrud->borrarID('9');
+    // echo "<pre>";
+    // var_dump($resultadoBorrarID);
+    // echo "</pre>";
+
+    // echo "<h2>Obtiene Todos Adopciones</h2>";
+    // $resultadoObitneTodos = $pruebaCrud2->obtieneTodos();
+    // echo "<pre>";
+    // var_dump($resultadoObitneTodos);
+    // echo "</pre>";
+
+    // echo "<h2>Borrar ID Adopciones</h2>";
+    // $resultadoBorrarID = $pruebaCrud2->borrarID('9');
+    // echo "<pre>";
+    // var_dump($resultadoBorrarID);
+    // echo "</pre>";
+
+    // echo "<h2>Obtiene Todos Adopciones</h2>";
+    // $resultadoObitneTodos = $pruebaCrud2->obtieneTodos();
+    // echo "<pre>";
+    // var_dump($resultadoObitneTodos);
+    // echo "</pre>";
 ?>
